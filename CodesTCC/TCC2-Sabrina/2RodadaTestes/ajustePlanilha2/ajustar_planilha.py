e06f4609b616640bb4dc8c38ee8fca19ca033c12
MariaAbritta <eduardaabritta@gmail.com>
2025-06-09 23:05:56 -0300

    Ajustando planilha de dados 2

---
+++ CodesTCC/TCC2-Sabrina/2RodadaTestes/ajustePlanilha2/ajustar_planilha.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+
+file_path = "planilha2.xlsx"
+
+data = pd.read_excel(file_path)
+
+# Substituindo os valores da coluna 'Ploidia' de 'euploide' e 'aneuploide' para 0 e 1
+data['Ploidia'] = data['Ploidia'].replace({'Euplóide': 0, 'Aneuplóide': 1})
+
+# Removendo a letra D da coluna Estágio para ficar somente números representado o dia
+data['Estágio'] = data['Estágio'].str.replace("D", "", regex=False)
+data['Estágio'] = pd.to_numeric(data['Estágio'], errors='coerce')
+
+# Substituindo a classificação Morfo pelo seu grupo de 1 a 4
+def classificar_morfo(valor):
+    prefixo = valor[0]
+    sufixo = valor[1:]
+
+    if sufixo == "AA" and prefixo in "3456":
+        return 1  # Excelente
+    elif sufixo in ["AB", "BA"] and prefixo in "3456":
+        return 2  # Bom
+    elif sufixo in ["BB", "AC", "CA"] and prefixo in "3456":
+        return 3  # Médio
+    else:
+        return 4  # Ruim
+
+# Aplicar a função diretamente na coluna 'Morfo'
+data['Morfo'] = data['Morfo'].apply(classificar_morfo)
+
+# Selecionando apenas as colunas numéricas
+numeric_columns = data.select_dtypes(include=[np.number])
+
+# Salvando em uma nova planilha
+output_file = "PlanilhaNumerica2.xlsx"
+numeric_columns.to_excel(output_file, index=False)
+
+print(f"Planilha somente com dados numéricos salva como: {output_file}")
